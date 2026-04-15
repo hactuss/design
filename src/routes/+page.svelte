8fb06f96eaed588610913aea11227f5db346c0cb
hactuss <hactuss@proton.me>
2026-04-15 15:10:07 +0200

    initial fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,29 +2,31 @@
 </script>
 
 <main>
-    <header>
-        <h1>Johannes Design</h1>
-        <div>
-            <button on:click={scrollTo(0, 50)}>scroll</button>
-        </div>
-    </header>
-    <main>
-        <p>
-            Ich mache für einen begrenzten Zeitraum digitale Designs in
-            verschiedensten Stilen. Sei es futuristisch, klassisch, simpel,
-            minimalistisch, ich kann es designen. Wie das ganze aussieht sehen
-            sie hier. Der hauptsächlich sehr futuristische Stil, den ich oft
-            verwende, wird durch the Designers Republic™ inspiriert.
-        </p>
+    {#if false}
+        <header>
+            <h1>Johannes Design</h1>
+            <div>
+                <button on:click={scrollTo(0, 50)}>scroll</button>
+            </div>
+        </header>
+        <main>
+            <p>
+                Ich mache für einen begrenzten Zeitraum digitale Designs in
+                verschiedensten Stilen. Sei es futuristisch, klassisch, simpel,
+                minimalistisch, ich kann es designen. Wie das ganze aussieht
+                sehen sie hier. Der hauptsächlich sehr futuristische Stil, den
+                ich oft verwende, wird durch the Designers Republic™ inspiriert.
+            </p>
 
-        <div class="portfolio">
-            <div class="gallery"></div>
-        </div>
-    </main>
-    <footer>
-        <h2>Kontakt</h2>
-        <p>holznerjohannes@proton.me</p>
-    </footer>
+            <div class="portfolio">
+                <div class="gallery"></div>
+            </div>
+        </main>
+        <footer>
+            <h2>Kontakt</h2>
+            <p>holznerjohannes@proton.me</p>
+        </footer>
+    {/if}
     <!-- --
     <h1>header</h1>
     <h2>header</h2>
