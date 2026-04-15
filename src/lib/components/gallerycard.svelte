9b3306de189cb03547a83d0aaaa388877bd27fe2
Hactuss <157395485+hactuss@users.noreply.github.com>
2026-04-15 15:07:47 +0200

    initial

---
+++ src/lib/components/gallerycard.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let image_url;
+</script>
+
+<main></main>
+
+<style>
+    main {
+        background: url({image_url});
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+</script>
+
+<main>
+    <header>
+        <h1>Johannes Design</h1>
+        <div>
+            <button on:click={scrollTo(0, 50)}>scroll</button>
+        </div>
+    </header>
+    <main>
+        <p>
+            Ich mache für einen begrenzten Zeitraum digitale Designs in
+            verschiedensten Stilen. Sei es futuristisch, klassisch, simpel,
+            minimalistisch, ich kann es designen. Wie das ganze aussieht sehen
+            sie hier. Der hauptsächlich sehr futuristische Stil, den ich oft
+            verwende, wird durch the Designers Republic™ inspiriert.
+        </p>
+
+        <div class="portfolio">
+            <div class="gallery"></div>
+        </div>
+    </main>
+    <footer>
+        <h2>Kontakt</h2>
+        <p>holznerjohannes@proton.me</p>
+    </footer>
+    <!-- --
+    <h1>header</h1>
+    <h2>header</h2>
+    <h3>header</h3>
+    <h4>header</h4>
+    <h5>header</h5>
+    <h6>header</h6>
+    <p>paragraph</p>
+    <button>button</button>-->
+</main>
+
+<style>
+</style>
